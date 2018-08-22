6dce48d4ff492836b3076fe81cad93fa2aace158
Hunter <kyhunter4@gmail.com>
2018-08-22 13:40:56 +0800

    Add files via upload

---
+++ ex1/warmUpExercise.py
@@ -0,0 +1,10 @@
+import numpy
+
+
+def get_matrix():
+    A = numpy.identity(5)
+    return A
+
+def pause():
+    pauseprogram = input("Press the <ENTER> key to continue...")
+    return pauseprogram
